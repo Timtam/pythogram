5cfc1f299d3e07d2971332d0a8b80f512bce0c5c
MX795 <maxha95@web.de>
2017-09-03 15:37:40 +0200

    added border to matplot panel and a little spacer between toolbar and figure

--- pythogram/matplotplanel.py
+++ pythogram/matplotplanel.py
@@ -8,9 +8,10 @@ from matplotlib.figure import Figure
 
 
 class MatplotPanel(wx.Panel):
-  def __init__(self, parent, size=(100, 100), t=numpy.arange(0.0, 10, 0.1),
+  def __init__(self, parent, size=(100, 100), style=wx.BORDER_SIMPLE,
+               t=numpy.arange(0.0, 10, 0.1),
                signal=numpy.sin(numpy.arange(0.0, 10, 0.1))):
-    wx.Panel.__init__(self, parent, size=size)
+    wx.Panel.__init__(self, parent, size=size, style=style)
     
     self.figure = Figure()
     self.axes = self.figure.add_subplot(111)
@@ -18,8 +19,10 @@ class MatplotPanel(wx.Panel):
     self.canvas = FigureCanvas(parent=self, id=wx.NewId(), figure=self.figure)
     self.toolbar = NavigationToolbar(self.canvas)
     
-    self.vbox = wx.BoxSizer(wx.VERTICAL)
-    self.SetSizer(self.vbox)
-    self.vbox.Add(item=self.toolbar, proportion=0, flag=wx.EXPAND)
-    self.vbox.Add(item=self.canvas, proportion=1,
-                  flag=wx.LEFT | wx.TOP | wx.GROW)
+    # vertical box with toolbar and figure canvas
+    self.v_box = wx.BoxSizer(wx.VERTICAL)
+    self.SetSizer(self.v_box)
+    self.v_box.Add(item=self.toolbar, proportion=0, flag=wx.EXPAND)
+    self.v_box.AddSpacer(4)
+    self.v_box.Add(item=self.canvas, proportion=1,
+                   flag=wx.LEFT | wx.TOP | wx.GROW)
