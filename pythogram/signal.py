a0a3ef28fbabb7a620b6dbd524aebd06f41c53ca
Toni Barth <toni.barth@gisa.de>
2017-09-12 13:37:03 +0200

    implemented signal addition * signals with same sample rate can be added together and will be normalized if needed

--- pythogram/signal.py
+++ pythogram/signal.py
@@ -28,3 +28,42 @@ class Signal(object):
     b, a = butter(4, [low, high], btype='band')
 
     return lfilter(b, a, self._signal)
+
+
+  def __add__(self, other):
+
+    # is the other object a signal too?
+    if not isinstance(other, Signal):
+      raise TypeError('non-signal cannot be added to signal')
+
+    # different sample rates?
+    if self.sample_rate != other.sample_rate:
+      raise TypeError('cannot add signals with different sample rates yet')
+
+    if self._signal.size < other._signal.size:
+      s1 = self._signal
+      s2 = other._signal
+    else:
+      s1 = other._signal
+      s2 = self._signal
+
+    if s1.size != s2.size:
+      # s1 is the shorter one and needs to be padded
+      s1 = np.pad(s1, (0, s2.size - s1.size), 'constant')
+
+    # generate a new signal as addition
+    s = s1+s2
+
+    # normalization
+    if np.max(np.abs(s), axis = 0)>1.0:
+      s /= np.max(np.abs(s), axis = 0)
+
+    # new class and attribute definition
+
+    os = Signal()
+    os.sample_rate = self.sample_rate
+    os.length = float(s.size)/self.sample_rate
+
+    os._signal = s
+
+    return os
