96bc08ed87d5652d4b5b0074f1c0fac303847d0c
Toni Barth <toni.barth@gisa.de>
2017-09-12 14:50:36 +0200

    sawtooth signals/sawtooth.py

---
+++ pythogram/signals/sawtooth.py
@@ -0,0 +1,21 @@
+import numpy as np
+from scipy.signal import sawtooth
+
+from ..signal import Signal
+
+
+
+class SawtoothSignal(Signal):
+  def __init__(self, freq=440.0, l=10.0, amp=1.0, srate=44100):
+    Signal.__init__(self)
+    self.sample_rate = srate
+    self.length = l
+    self.amplitude = amp
+    self.frequency = freq
+  
+  
+  @property
+  def _signal(self):
+    return self.amplitude * (sawtooth(2 * np.pi * self.frequency * np.linspace(
+      0, 1, self.sample_rate*self.length, endpoint=False))).astype(
+      np.float32)
