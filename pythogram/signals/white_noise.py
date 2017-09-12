c4988b12307cc1d2d748132eb268e1176ae1b212
Toni Barth <toni.barth@gisa.de>
2017-09-12 14:57:26 +0200

    we got some white noise

---
+++ pythogram/signals/white_noise.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+from ..signal import Signal
+
+
+
+class WhiteNoiseSignal(Signal):
+  def __init__(self, l=10.0, amp=1.0, srate=44100):
+    Signal.__init__(self)
+    self.sample_rate = srate
+    self.length = l
+    self.amplitude = amp
+  
+  
+  @property
+  def _signal(self):
+    return self.amplitude * np.random.normal(0, 1, size=int(self.sample_rate*self.length)).astype(np.float32)
+
